6cd998c1ad5548c6b3aadb5c835d2c3dea6ace9a
o108minmin <o108minmin@gmail.com>
2016-01-05 00:47:06 +0900

    Change vmath behavior (vmath)
    arg_type, math_type and return_type are setting of vmath behavior

--- pint/vmath.py
+++ pint/vmath.py
@@ -5,10 +5,40 @@ import math
 
 
 class vmath:
-    def sqrt(arg):
-        #TODO: Make more better if else
-        if arg.__class__.__name__ == "interval":
-            answer = arg.math.sqrt(arg)
+    arg_type = None
+    math_type = None
+    return_type = None
+    pn_class = ["interval"]
+
+    def __init__(self, arg_type = None, math_type = None,return_type = None):
+        self.arg_type = arg_type
+        self.math_type = math_type
+        self.return_type = return_type
+
+    def self_remove(args):
+        args_tmp = list(args)
+        if args[0].__class__.__name__ == "vmath":
+            args_tmp.pop(0)
+        return args_tmp
+
+    def sqrt(*args):
+        if args[0].__class__.__name__ == "vmath":
+            self = args[0]
         else:
-            answer = math.sqrt(arg)
+            self = vmath()
+        arg = vmath.self_remove(args)
+        if self.arg_type != None:
+            arg[0] = self.arg_type(arg[0])
+        if self.math_type != None:
+            tmp = self.math_type(arg[0])
+            arg_name = tmp.__class__.__name__
+        else:
+            arg_name = arg[0].__class__.__name__
+        pn_class_much = [x for x in vmath.pn_class if x == arg_name]
+        if len(pn_class_much) == 1:
+            answer = arg[0].math.sqrt(arg[0])
+        else:
+            answer = math.sqrt(arg[0])
+        if self.return_type != None:
+            answer = self.return_type(answer)
         return answer
