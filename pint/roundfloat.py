f349459838a190c654beb2f1e34e4319bd5e645b
o108minmin <o108minmin@asamikazuya-no-MacBook-Pro.local>
2016-06-29 01:12:18 +0900

    Add constants (roundfloat)

--- pint/roundfloat.py
+++ pint/roundfloat.py
@@ -5,20 +5,48 @@ import math
 from math import ldexp
 from pint.roundmode import roundmode
 '''
-    # Calculate addition, subtraction, multiplication, division and root with rounding mode.
+# Calculate addition, subtraction, multiplication, division and root with rounding mode.
 
-    # using exsample
-    from pint import roundfloat as rf
-    from pint import roundmode as rdm
+# using exsample
+from pint import roundfloat as rf
+from pint import roundmode as rdm
 
-    # Calculate 0.25 + 0.1 with rounding to up.
-    rf.rdadd(0.25, 0.1, rdm.up)
+# Calculate 0.25 + 0.1 with rounding to up.
+rf.rdadd(0.25, 0.1, rdm.up)
 
-    # Calculate 0.25 - 0.1 with rounding to down
-    rf.rdsub(0.25, 0.1, rdm.down)
+# Calculate 0.25 - 0.1 with rounding to down
+rf.rdsub(0.25, 0.1, rdm.down)
 '''
+
+
+'''
+constants
+
+L = ldexp
+M = minus
+P = plus
+LM969 = ldexp(1, -969)
+'''
+L27P1 = ldexp(1, 27) + 1
+L28 = ldexp(1, 28)
+LM28 = ldexp(1, -28)
+L53 = ldexp(1, 53)
+LM53 = ldexp(1, -53)
+L105 = ldexp(1, 105)
+LM105 = ldexp(1, -105)
+L106 = ldexp(1, 106)
+L537 = ldexp(1, 537)
+L918 = ldexp(1, 918)
+L996 = ldexp(1, 996)
+LM969 = ldexp(1, -969)
+LM1021 = ldexp(1, -1021)
+L1023 = ldexp(1, 1023)
+LM1023 = ldexp(1, -1023)
+LM1074 = ldexp(1, -1074)
+LM53PLM105 = (ldexp(1, -53) + ldexp(1, -105))
+
 def split(a):
-    tmp = a * (ldexp(1, 27) + 1)
+    tmp = a * (L27P1)
     x = tmp - (tmp - a)
     y = a - x
     return x, y
@@ -26,31 +54,31 @@ def split(a):
 # succ and pred by S. M. Rump:
 def succ(a):
     abs_a = abs(a)
-    if abs_a >= ldexp(1, -969):
-        return a + abs_a * (ldexp(1, -53) + ldexp(1, -105))
-    if abs_a <= ldexp(1, -1021):
-        return a + ldexp(1, -1074)
-    c = ldexp(1, 53) * a
-    e = (ldexp(1, -53) + ldexp(1, -105)) * abs(c)
-    return (c + e) * ldexp(1, -53)
+    if abs_a >= LM969:
+        return a + abs_a * LM53PLM105
+    if abs_a <= LM1021:
+        return a + LM1074
+    c = L53 * a
+    e = LM53PLM105 * abs(c)
+    return (c + e) * LM53
 
 def pred(a):
     abs_a = abs(a)
-    if abs_a >= ldexp(1, -969):
-        return a - abs_a * (ldexp(1, -53) + ldexp(1, -105))
-    if abs_a <= ldexp(1, -1021):
-        return a - ldexp(1, -1074)
-    c = ldexp(1, 53) * a
-    e = (ldexp(1, -53) + ldexp(1, -105)) * abs(c)
-    return (c - e) * ldexp(1, -53)
+    if abs_a >= LM969:
+        return a - abs_a * LM53PLM105
+    if abs_a <= LM1021:
+        return a - LM1074
+    c = L53 * a
+    e = LM53PLM105 * abs(c)
+    return (c - e) * LM53
 
 # by Masahide Kashiwagi
 # http://verifiedby.me/adiary/029
 
 def fasttwosum(a, b):
     x = a + b
-	tmp = x - a
-	y = b - tmp
+    tmp = x - a
+    y = b - tmp
     return x, y
 
 def twosum(a, b):
@@ -69,18 +97,18 @@ def twoproduct(a, b):
     arg1 = a
     arg2 = b
     x = arg1 * arg2
-    if abs(arg1) > 2.**996:
-        arg1fix = arg1 * 2.**(-28)
-        arg2fix = arg2 * 2.**(28)
-    elif abs(arg2) > 2.**(996):
-        arg1fix = arg1 * 2.**(28)
-        arg2fix = arg2 * 2.**(-28)
+    if abs(arg1) > L996:
+        arg1fix = arg1 * LM28
+        arg2fix = arg2 * L28
+    elif abs(arg2) > L996:
+        arg1fix = arg1 * L28
+        arg2fix = arg2 * LM28
     else:
         arg1fix = arg1
         arg2fix = arg2
     aH, aL = split(arg1fix)
     bH, bL = split(arg2fix)
-    if abs(x) > 2.**1023:
+    if abs(x) > L1023:
         y = aL * bL - ((((x * 0.5) - (aH * 0.5) * bH) * 2. - aL * bH) - aH * bL)
     else:
         y = aL * bL - (((x - aH * bH) - aL * bH) - aH * bL)
@@ -127,7 +155,7 @@ def rdmul(a, b, rmode=roundmode.nearest):
                 return x
             else:
                 return -float_info.max
-        if abs(x) >= 2.**(-969):
+        if abs(x) >= LM969:
             if y > 0:
                 x = succ(x)
     elif rmode == roundmode.down:
@@ -139,12 +167,12 @@ def rdmul(a, b, rmode=roundmode.nearest):
         elif x == -float('inf'):
             return x
 
-        if abs(x) >= 2.**(-969):
+        if abs(x) >= LM969:
             if y < 0.:
                 return pred(x)
         else:
-            s1, s2 = twoproduct(arg1 * 2.**537, arg2 * 2.**537)
-            t = (x * 2.**537) * 2.**537
+            s1, s2 = twoproduct(arg1 * L537, arg2 * L537)
+            t = (x * L537) * L537
             if t > s1 or (t == s1 and s2 < 0.):
                 return pred(x)
     return x
@@ -164,15 +192,15 @@ def rddiv(a, b, rmode=roundmode.nearest):
         else:
             arg1fix = arg1
             arg2fix = arg2
-        if abs(arg1fix) < 2.**(-969):
-            if abs(arg2fix) < 2.**918:
-                arg1fix *= 2.**105
-                arg2fix *= 2.**105
+        if abs(arg1fix) < LM969:
+            if abs(arg2fix) < L918:
+                arg1fix *= L105
+                arg2fix *= L105
             else:
                 if arg1fix < 0.:
                     return 0.
                 else:
-                    return 2.**(-1074)
+                    return LM1074
         d = arg1fix / arg2fix
         if d == float('inf'):
             return d
@@ -194,13 +222,13 @@ def rddiv(a, b, rmode=roundmode.nearest):
         else:
             arg1fix = arg1
             arg2fix = arg2
-        if abs(arg1fix) < 2.**(-969):
-            if abs(arg2fix) < 2.**918:
-                arg1fix *= 2.**105
-                arg2fix *= 2.**105
+        if abs(arg1fix) < LM969:
+            if abs(arg2fix) < L918:
+                arg1fix *= L105
+                arg2fix *= L105
             else:
                 if arg1fix < 0.:
-                    return -2.**(-1074)
+                    return -LM1074
                 else:
                     return 0
         d = arg1fix / arg2fix
@@ -217,9 +245,9 @@ def rdsqrt(x, rmode=roundmode.nearest):
     arg = x
     d = math.sqrt(arg)
     if rmode == roundmode.up:
-        if arg < 2.**(-969):
-            a2 = arg * 2.**106
-            d2 = d * 2.**53
+        if arg < LM969:
+            a2 = arg * L106
+            d2 = d * L53
             x, y = twoproduct(d2, d2)
             if x < a2 or (x == a2 and y < 0.):
                 d = succ(d)
@@ -227,9 +255,9 @@ def rdsqrt(x, rmode=roundmode.nearest):
         if x < arg or (x == arg and y < 0.):
             d = succ(d)
     if rmode == roundmode.down:
-        if arg < 2.**(-969):
-            a2 = arg * 2.**106
-            d2 = d * 2.**53
+        if arg < LM969:
+            a2 = arg * L106
+            d2 = d * L53
             x, y = twoproduct(d2, d2)
             if x > a2 or (x == a2 and y > 0.):
                 d = pred(d)
